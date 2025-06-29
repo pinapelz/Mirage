864ce67d89c77d8ef9c3361f80d619853abcf91c
Pinapelz <yukais@pinapelz.com>
2025-06-29 11:41:51 -0700

    backend: migrate to postgres

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const port = 5000;
 
 app.use(cors({
   origin: [
-    'http://localhost:5173', // Vite dev server default port
+    process.env.FRONTEND_URL || "http://localhost:5173"
   ],
   credentials: true
 }));
