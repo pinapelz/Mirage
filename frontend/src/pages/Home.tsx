b42742839af8e126520acd17cdd3aa6303634d60
Pinapelz <yukais@pinapelz.com>
2025-08-27 16:17:00 -0700

    fix incorrect internal page value for homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -72,7 +72,7 @@ const Home = () => {
 
   return (
     <div className="min-h-screen bg-slate-950">
-      <NavBar user={user} handleLogout={handleLogout} currentPage="import"/>
+      <NavBar user={user} handleLogout={handleLogout} currentPage="home"/>
 
       {/* Main Content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
